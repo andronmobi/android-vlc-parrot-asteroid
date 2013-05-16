0048cf70d6eac255cef8857e0bcdaa12eb4b9d8e
Andrei Mandychev <andron.mobi@gmail.com>
2013-05-16 15:31:17 +0200

    Update VLC hash

--- compile.sh
+++ compile.sh
@@ -80,7 +80,7 @@ export PATH=${NDK_TOOLCHAIN_PATH}:${PATH}
 ANDROID_PATH="`pwd`"
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=2a74bb484
+TESTED_HASH=1d7ce3ffe
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone https://github.com/andronmobi/vlc-parrot-asteroid.git vlc
